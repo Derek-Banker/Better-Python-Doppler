11479d744fb46f97041fe1ee2192f33fff3098f7
Derek (Work) <dbanker@aerospheremonitoring.com>
2025-07-28 12:11:19 -0700

    Added: Effective overload for Secrets.update()
    Added parameters to make updating a single secret easier.
    Updated [example_secrets.py] to reflect changes.

--- examples/example_secrets.py
+++ examples/example_secrets.py
@@ -4,10 +4,18 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+from datetime import datetime as DateTime
+
 SERVICE_TOKEN = os.getenv("SERVICE_TOKEN")
 PROJECT_NAME = os.getenv("PROJECT_NAME")
 CONFIG_NAME = os.getenv("CONFIG_NAME")
-SECRET_NAME = os.getenv("SECRET_NAME")
+
+var_list = [SERVICE_TOKEN, PROJECT_NAME, CONFIG_NAME]
+
+if (    SERVICE_TOKEN   is None  
+    or  PROJECT_NAME    is None 
+    or  CONFIG_NAME     is None):
+    raise ValueError
 
 # === BOILER PLATE SUPPORT ==============
 
@@ -19,4 +27,12 @@ names = doppler.Secrets.list_names(PROJECT_NAME, CONFIG_NAME)
 
 list_secrets = doppler.Secrets.list(PROJECT_NAME, CONFIG_NAME)
 
-print(doppler.Secrets.get(PROJECT_NAME, CONFIG_NAME, SECRET_NAME).value.raw)
+print(doppler.Secrets.get(PROJECT_NAME, CONFIG_NAME, "TEST_GET").value.raw)
+
+doppler.Secrets.update(PROJECT_NAME, CONFIG_NAME, secrets={"TEST_SET": DateTime.strftime(DateTime.now(), "%Y-%m-%d %H:%M:%S")})
+
+print(doppler.Secrets.get(PROJECT_NAME, CONFIG_NAME, "TEST_SET").value.raw)
+
+doppler.Secrets.update(PROJECT_NAME, CONFIG_NAME, "TEST_SET", DateTime.strftime(DateTime.now(), "%Y-%m-%d %H:%M:%S"))
+
+print(doppler.Secrets.get(PROJECT_NAME, CONFIG_NAME, "TEST_SET").value.raw)
